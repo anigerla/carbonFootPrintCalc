0941811e1b56575194c687ae6f2824aec9855de2
Regina <regina.zaripova@gmail.com>
2019-06-02 16:27:49 -0400

    Updates

---
+++ precache-manifest.e3a9b460097fd7a375516260346cda5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64b0c223d3f18225ab3a",
+    "url": "/capstone/static/js/runtime~main.64b0c223.js"
+  },
+  {
+    "revision": "7122bfd5cc2366462e6e",
+    "url": "/capstone/static/js/main.7122bfd5.chunk.js"
+  },
+  {
+    "revision": "007e95d96e2fb1a2f437",
+    "url": "/capstone/static/js/1.007e95d9.chunk.js"
+  },
+  {
+    "revision": "7122bfd5cc2366462e6e",
+    "url": "/capstone/static/css/main.234c06c2.chunk.css"
+  },
+  {
+    "revision": "a7c597aefb3e11ce39a7c961724867a2",
+    "url": "/capstone/index.html"
+  }
+];
